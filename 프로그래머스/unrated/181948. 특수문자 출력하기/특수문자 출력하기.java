dd97d9c80c3e8b22f6b5d845f0160af875f35c5b
이건희 <105201451+dnzp75@users.noreply.github.com>
2023-10-18 22:16:22 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 147.24 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+       System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+    
+} //문제가 되는 특수문자 \ -> \ \     "-> \"
