2b4e8a5d75b6b2ed770de3eafc3fbd85c040fc76
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-02-13 14:59:38 +0900

    [Silver III] Title: 통계학, Time: 632 ms, Memory: 53216 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main{
     public static void main(String[] args) throws IOException {
