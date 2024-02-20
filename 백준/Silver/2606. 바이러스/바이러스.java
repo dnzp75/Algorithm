6fdf8ad969ac56de2f881ec9f7b2f217f28217e7
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-02-20 22:19:09 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,14 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
     static List<Integer>[] network; // 네트워크 연결 정보를 저장할 리스트 배열
     static boolean[] visited; // 각 컴퓨터 방문 여부를 저장할 배열
-    static int infectedCount = 0; // 감염된 컴퓨터의 수
+    static int count = 0; // 감염된 컴퓨터의 수
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(); // 컴퓨터의 수
-        int m = scanner.nextInt(); // 연결된 컴퓨터 쌍의 수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+        int m = Integer.parseInt(br.readLine()); // 연결된 컴퓨터 쌍의 수
 
         network = new ArrayList[n + 1]; // 1번부터 시작하므로 n+1 크기로 초기화
         visited = new boolean[n + 1]; // 방문 여부 배열 초기화
@@ -19,8 +22,9 @@ public class Main {
 
         // 네트워크 연결 정보 입력
         for (int i = 0; i < m; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             // 양방향 연결
             network[a].add(b);
             network[b].add(a);
@@ -30,7 +34,7 @@ public class Main {
         dfs(1);
 
         // 1번 컴퓨터를 제외한 감염된 컴퓨터의 수 출력
-        System.out.println(infectedCount - 1);
+        System.out.println(count - 1);
     }
 
     // DFS 메소드
@@ -39,10 +43,11 @@ public class Main {
             return; // 이미 방문한 컴퓨터라면 더 이상 탐색하지 않음
         }
         visited[start] = true; // 방문 표시
-        infectedCount++; // 감염된 컴퓨터 수 증가
+        count++; // 감염된 컴퓨터 수 증가
 
         // 현재 컴퓨터와 연결된 모든 컴퓨터에 대해 DFS 탐색
-        for (int next : network[start]) {
+        for (int i = 0; i < network[start].size(); i++) {
+            int next = network[start].get(i);
             if (!visited[next]) {
                 dfs(next);
             }
