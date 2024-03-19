393a2a34f61719160a26a0e477be8129c0668aab
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-03-19 20:18:00 +0900

    [Silver III] Title: 계단 오르기, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-// 계단 오르기
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 계단의 개수 입력
 
-        // 각 계단 점수
+        // 각 계단 점수 입력
         int[] stair = new int[n + 2];
         for (int i = 1; i <= n; i++) {
-            stair[i] = sc.nextInt();
+            stair[i] = Integer.parseInt(br.readLine());
         }
 
-        // 해당 계단 위치까지의 최고 점수
+        // 해당 계단 위치까지의 최고 점수를 계산
         int[] score = new int[n + 2];
         score[1] = stair[1];
         score[2] = stair[1] + stair[2];
@@ -20,6 +21,6 @@ public class Main {
             score[i] = Math.max(score[i - 3] + stair[i - 1], score[i - 2]) + stair[i];
         }
 
-        System.out.println(score[n]);
+        System.out.println(score[n]); // 최종 점수 출력
     }
 }
