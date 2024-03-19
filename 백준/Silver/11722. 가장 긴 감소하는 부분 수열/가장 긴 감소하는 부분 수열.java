2db4b59b05dd690ce95050ac9d93df1076e92c9b
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-03-20 00:14:29 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 148 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,39 +1,38 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stringTokenizer;
 
-        int num = Integer.parseInt(br.readLine());
-        stringTokenizer = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] arr = new int[num];
-        int[] dp = new int[num];
+        int[] arr = new int[N];
+        int[] dp = new int[N];
 
         dp[0] = 1;
 
-        for(int i=0; i<num; i++)
-            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        for(int i=1; i<num; i++){
+        for (int i = 1; i < N; i++) {
             int max = 0;
-            for(int j=0; j<i; j++){
-                if(arr[i] < arr[j])
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) {
                     max = Math.max(dp[j], max);
+                }
             }
             dp[i] = max + 1;
         }
 
         Arrays.sort(dp);
 
-        bw.write(dp[num-1]+ "\n");
-        bw.flush();
+        System.out.println(dp[N - 1]);
     }
 }
