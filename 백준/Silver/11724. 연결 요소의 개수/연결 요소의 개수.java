d83db061a8f7fe3d9aa7958dbf8cfe5fbfb86bcf
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-04-02 21:47:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 508 ms, Memory: 111684 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,54 +1,57 @@
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] adjList;
-    static boolean[] visited;
-    static int N, M;
+
+    static int[] parent; // 각 노드의 부모 노드를 저장하는 배열
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        adjList = new ArrayList[N + 1];
+        parent = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            adjList[i] = new ArrayList<>();
+            parent[i] = i; // 초기에 각 노드의 부모는 자기 자신
         }
-
-        visited = new boolean[N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-
-            adjList[u].add(v);
-            adjList[v].add(u); // 무방향 그래프이므로 양방향으로 추가
+            union(u, v); // 각 간선에 대해 두 정점을 연결(Union)
         }
 
         int componentCount = 0;
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                componentCount++; // 새로운 연결 요소 발견
+            if (find(i) == i) { // 각 노드에 대해 루트 노드가 자기 자신인 경우, 즉 연결 요소의 대표자인 경우
+                componentCount++;
             }
         }
 
-        System.out.println(componentCount);
+        System.out.println(componentCount); // 연결 요소의 개수 출력
     }
 
-    static void dfs(int v) {
-        visited[v] = true;
-        for (int adj : adjList[v]) {
-            if (!visited[adj]) {
-                dfs(adj);
-            }
+    // Find 함수: 특정 원소가 속한 집합의 루트 노드를 찾음
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        } else {
+            return parent[x] = find(parent[x]);
         }
     }
+
+    // Union 함수: 두 원소가 속한 집합을 합침
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
 }
