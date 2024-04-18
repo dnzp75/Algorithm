9a61d6586a6634c456764bdb434efece65cfdd2a
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-04-18 17:51:39 +0900

    [Silver II] Title: 마인크래프트, Time: 400 ms, Memory: 33956 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,63 +1,50 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
-    int B = Integer.parseInt(st.nextToken());
-    int[][] arr = new int[N][M];
-    int min = 256;
-    int max = 0;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-    for(int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      for(int j = 0; j < M; j++) {
-        arr[i][j] = Integer.parseInt(st.nextToken());
-        if(min > arr[i][j])
-          min = arr[i][j];
-        if(max < arr[i][j])
-          max = arr[i][j];
-      }
-    }
-
-    //time은 최소시간을 저장 할 변수, 풀이에 적었듯이
-    //올 수 있는 값은 6400만정도로 예상 되기에 넉넉히 9999만을 저장.
-    int time = 99999999;
-    int high = 0;
-    //만틀 층 i
-    for(int i = min; i <= max; i++) {
-      int count = 0;
-      int block = B;
-      //좌표 j와 k
-      for(int j = 0; j < N; j++) {
-        for(int k = 0; k < M; k++) {
-          //현재 좌표의 층이 만들 층보다 높으면 제거하는데, 블록은 제거한 만큼 추가, 시간은 두배로
-          if(i < arr[j][k]) {
-            count += ((arr[j][k] - i) * 2);
-            block += (arr[j][k] - i);
-            //낮을 경우 블록은 제거, 시간은 1배
-          }else {
-            count += (i - arr[j][k]);
-            block -= (i - arr[j][k]);
-          }
+        int[] heightCount = new int[257];  // 높이별 카운트 배열
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                int height = Integer.parseInt(st.nextToken());
+                heightCount[height]++;
+            }
         }
-      }
-      //블록의 개수가 음수가 되면 반복문 종료
-      if(block < 0) break;
 
-      if(time >= count) {
-        time = count;
-        high = i;
-      }
+        int minTime = Integer.MAX_VALUE;
+        int resultHeight = -1;
+
+        for (int targetHeight = 0; targetHeight <= 256; targetHeight++) {
+            int removeBlocks = 0;
+            int addBlocks = 0;
+
+            for (int h = 0; h <= 256; h++) {
+                if (h < targetHeight) {
+                    addBlocks += (targetHeight - h) * heightCount[h];
+                } else if (h > targetHeight) {
+                    removeBlocks += (h - targetHeight) * heightCount[h];
+                }
+            }
+
+            if (removeBlocks + B >= addBlocks) {  // 충분한 블록이 있는지 확인
+                int time = 2 * removeBlocks + addBlocks;
+                if (time < minTime || (time == minTime && targetHeight > resultHeight)) {
+                    minTime = time;
+                    resultHeight = targetHeight;
+                }
+            }
+        }
+
+        System.out.println(minTime + " " + resultHeight);
     }
-    System.out.println(time + " " + high);
-  }
-
 }
