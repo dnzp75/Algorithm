903245486914f6d847806e17a63829a5f540ceb1
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-05-16 20:36:19 +0900

    [Silver I] Title: 점프, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -1,48 +1,39 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer stringTokenizer;
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] board = new int[N][N];
-        long[][] dp = new long[N][N];
+        int num = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        long[][] dp = new long[num + 1][num + 1];
 
-        dp[0][0] = 1; // 시작점
+        dp[1][1] = 1;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i == N - 1 && j == N - 1) {
-                    break; // 도착점이면 탈출
-                }
+        for (int i = 1; i <= num; i++) {
+            stringTokenizer = new StringTokenizer(br.readLine());
 
-                int jump = board[i][j];
-                if (jump == 0) {
-                    continue; // 0이면 점프 불가
-                }
+            for (int j = 1; j <= num; j++) {
+                int moveLength = Integer.parseInt(stringTokenizer.nextToken());
 
-                if (i + jump < N) {
-                    dp[i + jump][j] += dp[i][j]; // 아래로 점프
-                }
-                if (j + jump < N) {
-                    dp[i][j + jump] += dp[i][j]; // 오른쪽으로 점프
+                if (dp[i][j] >= 1 && moveLength != 0) {
+                    if (j + moveLength <= num) {
+                        dp[i][j + moveLength] += dp[i][j];
+                    }
+
+                    if (i + moveLength <= num) {
+                        dp[i + moveLength][j] += dp[i][j];
+                    }
                 }
             }
         }
+        System.out.println(dp[num][num]);
 
-        System.out.println(dp[N - 1][N - 1]);
     }
 }
