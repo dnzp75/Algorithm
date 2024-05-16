851e64c8ed72b2c3cf9042d890795864de13b677
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-05-16 20:13:18 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 132 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
@@ -1,28 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-public class Main{
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-    	int[] dp = new int[n];
-    	
-    	for(int i = 1; i <= m/2; i++) {
-    		for(int j = i+1; j < m; j++) {
-    			dp[0] = i;
-    			dp[1] = j;
-    			for(int k = 2; k < n; k++) {
-    				dp[k] = dp[k-1] + dp[k-2];
-    			}
-    			if(dp[n-1] == m) {
-    				System.out.println(dp[0]);
-    				System.out.println(dp[1]);
-    				System.exit(0);
-    			}
-    		}
-    	}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int D = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[D];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // DP를 사용하여 피보나치 수열의 계수를 계산
+        for (int i = 2; i < D; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        // A와 B를 찾기 위한 반복문
+        boolean found = false;
+        int A = 1;
+        while (!found) {
+            if ((K - dp[D - 3] * A) % dp[D - 2] == 0) {
+                int B = (K - dp[D - 3] * A) / dp[D - 2];
+                System.out.println(A);
+                System.out.println(B);
+                found = true;
+            }
+            A++;
+        }
     }
 }
