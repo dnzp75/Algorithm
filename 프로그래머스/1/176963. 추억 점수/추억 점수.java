9270c362cc032689faffd68f54aa5da6f97b573a
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-06-20 23:19:38 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        // 각 인물의 이름과 그리움 점수를 저장하는 해시맵 생성
-        HashMap<String, Integer> score = new HashMap<>();
-        for(int i = 0; i < name.length; i++){
-            score.put(name[i], yearning[i]);
-        }
-        
-        // 각 사진의 추억 점수를 저장할 배열 초기화
         int[] answer = new int[photo.length];
         
-        // 각 사진에 대해 추억 점수 계산
-        for(int i = 0; i < photo.length; i++){
-            int photoScore = 0;
-            for(int j = 0; j < photo[i].length; j++){
-                String person = photo[i][j];
-                if(score.containsKey(person)) {
-                    photoScore += score.get(person);
-                }
-            }
-            answer[i] = photoScore;
+        HashMap<String,Integer> hashmap = new HashMap<>();
+        
+        
+        for(int i=0; i<name.length; i++){
+            hashmap.put(name[i],yearning[i]);
         }
         
+        for(int i=0; i<photo.length; i++){
+            String[] persons = photo[i];
+            int score=0;
+            for(int j=0; j<persons.length; j++){
+                String person = persons[j];
+                
+                if(hashmap.containsKey(person)){
+                    score+=hashmap.get(person);
+                }
+            }
+            
+            answer[i]=score;
+        }
         return answer;
     }
 }
