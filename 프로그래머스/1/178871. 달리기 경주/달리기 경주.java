2d14f909d6dedcf750ef896c9f849c3428f39319
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-06-20 22:55:18 +0900

    [level 1] Title: 달리기 경주, Time: 0.05 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
         
-        HashMap<String, Integer> ranking = new HashMap<>();
+        HashMap<String, Integer> hashmap = new HashMap<>();
         
-        for(int i=0; i<players.length; i++){
-            ranking.put(players[i],i);
+        for(int i=0; i<players.length; i++) {
+            hashmap.put(players[i], i);
         }
         
-            
-        for( String player : callings ) {
-            int playerRanking = ranking.get(player);
+        for(String player : callings){
+            int playerRanking = hashmap.get(player);
             
             String frontPlayer = players[playerRanking-1];
             
-            ranking.replace(frontPlayer, playerRanking);
+            players[playerRanking-1] = player;
             players[playerRanking] = frontPlayer;
             
-            ranking.replace(player, playerRanking-1);
-            players[playerRanking-1] = player;
-             }
-                return players;
-
+            hashmap.put(player, playerRanking-1);
+            hashmap.put(frontPlayer, playerRanking);
+            
         }
+        return players;
+    }
 }
