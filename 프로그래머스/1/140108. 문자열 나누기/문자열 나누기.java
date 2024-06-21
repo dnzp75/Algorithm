02dfbd9ec527875a87ef5c0aa8421856b3266a17
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-06-22 01:11:41 +0900

    [level 1] Title: 문자열 나누기, Time: 1.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,33 +1,34 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int countX = 0;
-        int countNotX = 0;
-        char x = s.charAt(0); // 첫 글자를 x로 초기화
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == x) {
-                countX++;
+        int count=0;
+        int countNotX=0;
+        char x = s.charAt(0);
+        
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == x){
+                count++;
             } else {
                 countNotX++;
             }
-
-            if (countX == countNotX) {
+            
+            if(count == countNotX){
                 answer++;
-                // 카운트 초기화 및 다음 부분 문자열의 첫 글자를 설정
-                if (i + 1 < s.length()) {
-                    x = s.charAt(i + 1);
+                
+                if(i+1<s.length()){
+                    x=s.charAt(i+1);
                 }
-                countX = 0;
-                countNotX = 0;
+                
+                count =0;
+                countNotX=0;
             }
+             
         }
-
-        // 남은 부분 문자열이 있을 경우
-        if (countX != 0 || countNotX != 0) {
+        
+        if(count != 0 || countNotX != 0){
             answer++;
         }
-
         return answer;
     }
 }
