dd138b47ac745178369b0741f274ab410de66d8d
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-06-24 17:29:34 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,20 +1,14 @@
-import java.util.ArrayList;
-
 class Solution {
     public int solution(int[] numbers) {
-        
         int answer = 0;
-        ArrayList<Integer> list = new ArrayList<>();
-        
+        int num=0;
+                
         for(int i=0; i<numbers.length; i++){
-            list.add(numbers[i]); // [1,2,3,4,6,7,8,0]	
-            }
-        for(int i=0; i<10; i++){
-            if(list.contains(i)){
-                continue;
-            }
-            answer+= i;
+            num += numbers[i];
         }
+        
+        answer = 45-num;
+        
         return answer;
     }
 }
