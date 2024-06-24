195af597283e9d8a04181be14d93e40964e61b25
이건희 <105201451+dnzp75@users.noreply.github.com>
2024-06-24 14:41:09 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -9,15 +9,16 @@ class Solution {
         for(int i=0; i<ingredient.length; i++){
             list.add(ingredient[i]);
             
-            if(list.size() >= 4
-              &&list.get(list.size()-4) == 1
+            if(list.size()>=4
+              && list.get(list.size()-4) == 1
               && list.get(list.size()-3) == 2
-              && list.get(list.size()-2) ==3
+              && list.get(list.size()-2) == 3
               && list.get(list.size()-1) == 1){
                 answer++;
                 for(int j=0; j<4; j++){
                     list.remove(list.size()-1);
                 }
+                
             }
         }
         return answer;
