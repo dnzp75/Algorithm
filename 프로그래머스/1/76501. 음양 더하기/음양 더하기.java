10e6db22591f663f0f1e51fbc5b3e7c79a5d6713
이건희 <105201451+dnzp75@users.noreply.github.com>
2026-06-22 21:31:40 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,13 +2,15 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         
-        for(int i=0; i<absolutes.length; i++){
-            if(signs[i] == true){
+        for(int i=0; i<signs.length; i++){
+            if(signs[i] == false){
+                absolutes[i] = absolutes[i] - (absolutes[i]*2);
+                answer +=  absolutes[i];
+            } else if(signs[i] == true){
                 answer += absolutes[i];
-            } else {
-                answer -=absolutes[i];
             }
         }
+        
         return answer;
     }
 }
